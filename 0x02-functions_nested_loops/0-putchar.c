f3c685873d09e6cb6f95118ad93dc0715fe53dee
Mosesbwire <mosesdev92@gmail.com>
2022-12-14 04:09:37 +0000

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *main - entry point
+  *
+  *prints to std out
+  *
+  *Return: 0 (success)
+  *
+  */
+
+int main(void)
+{
+	_putchar("_putchar");
+
+	_putchar("\n");
+
+	return (0);
+}

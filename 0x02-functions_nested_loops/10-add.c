e70df0f3ba8251663ff1531bdbe6bf64df518194
Mosesbwire <mosesdev92@gmail.com>
2022-12-14 12:27:36 +0000

    complete task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+  * add - adds two ints
+  * @a: parameter one
+  * @b: parameter two
+  * Return: sum of two values
+  *
+  */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}

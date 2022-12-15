d0a22975f85f27ce70078e139c468c7a4ecd31b2
Mosesbwire <mosesdev92@gmail.com>
2022-12-15 06:58:07 +0000

    complete task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+  * mul - multiply two ints return results
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: multiplication value
+  */
+
+int mul(int a, int b)
+{
+	int answer;
+
+	answer = a * b;
+
+	return (answer);
+}

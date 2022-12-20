03d9b6bddcdf2b756073ae3a16247e8a93450470
Mosesbwire <mosesdev92@gmail.com>
2022-12-20 12:49:28 +0000

    complete task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+/**
+  * _strcpy - copy content from one string to another
+  *
+  * @dest: argument to get contents
+  * @src: argument to copy content from
+  * Return: pointer to @dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = strlen(src);
+	int x;
+	char *ptr_dest = dest;
+
+	for (x = 0; x < length; x++)
+	{
+		*(dest + x) = *(src + x);
+	}
+
+	return (ptr_dest);
+
+}

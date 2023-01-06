8e70724f7334fa126a50ea2c6eeb0bfc95d1841c
Mosesbwire <mosesdev92@gmail.com>
2023-01-06 12:56:07 +0000

    complete advanced task 100-palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,58 @@
+/**
+  * _strlen - calculates length of a string
+  * @s: string to get length
+  * Return: integer representing length of string
+  */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen(s + 1));
+}
+
+/**
+  * check_palindrome - checks if string is helper, recursively
+  * @s: string to check if is palindrome
+  * @x: string @s in reverse
+  * Return: 1 if string is palindrome otherwise 0
+  */
+
+int check_palindrome(char *s, char *x)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+
+	if (*s != *x)
+	{
+		return (0);
+	}
+
+	return (check_palindrome(s + 1, x - 1));
+
+}
+
+/**
+  * is_palindrome - checks if string is a palindrome
+  * @s: string to check if is palindrome
+  * Return: 1 if string is palindrome otherwise 0
+  */
+
+int is_palindrome(char *s)
+{
+	int x = _strlen(s);
+
+	if (x == 1 && *s == ' ')
+	{
+		return (1);
+	}
+
+	x--;
+
+	return (check_palindrome(s, s + x));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
 int is_prime_number(int n);
-
+int is_palindrome(char *s);
 
 #endif

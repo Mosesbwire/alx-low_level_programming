0118f79def860d45449e09ba6489818498198cee
Mosesbwire <mosesdev92@gmail.com>
2023-01-18 02:47:15 +0000

    replace standard lib functions with own function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,34 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+/**
+ * len - find length of string
+ * @str: string
+ * Return: length
+ */
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		;
+	return (i);
+}
+/**
+ * strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= len(src); i++)
+		dest[i] = src[i];
+	return (dest);
+}
 
 /**
   * new_dog - creates a new dog
@@ -22,13 +50,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->age = age;
 	if (name != NULL)
 	{
-		cp_name = malloc(strlen(name) + 1);
+		cp_name = malloc(len(name) + 1);
 		if (cp_name == NULL)
 		{
 			free(dog);
 			return (NULL);
 		}
-		dog->name = strcpy(cp_name, name);
+		dog->name = _strcpy(cp_name, name);
 	} else
 	{
 		dog->name = NULL;
@@ -36,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (owner != NULL)
 	{
-		cp_owner = malloc(strlen(owner) + 1);
+		cp_owner = malloc(len(owner) + 1);
 		if (cp_owner == NULL)
 		{
 			free(cp_name);
@@ -44,7 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			return (NULL);
 
 		}
-		dog->owner = strcpy(cp_owner, owner);
+		dog->owner = _strcpy(cp_owner, owner);
 	} else
 	{
 		dog->owner = NULL;

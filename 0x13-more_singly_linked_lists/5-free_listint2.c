a976476b84a9b4cdf1109bbc6a18012afbb46628
Mosesbwire <mosesdev92@gmail.com>
2023-02-02 06:37:42 +0000

    fix task 5 - bring functionality to free mem in file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,13 @@
 
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
+	listint_t *temp_node = *head;
+
+	while (temp_node != NULL)
+	{
+		*head = *head->next;
+		free(temp_node);
+		temp_node = *head;
+	}
 	*head = NULL;
 }

71b9a9873c6858753a6e5bdc4c050aeae489d16d
Mosesbwire <mosesdev92@gmail.com>
2023-02-02 06:53:46 +0000

    add missing semi-colon task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				return (NULL);
 			}
-			new_node->n = n
+			new_node->n = n;
 			new_node->next = temp_node->next;
 			temp_node->next = new_node;
 			return (new_node);

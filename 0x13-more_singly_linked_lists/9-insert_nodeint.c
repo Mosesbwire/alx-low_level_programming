435dd808a154e2d7da039c7a75c7974883f1e3ea
Mosesbwire <mosesdev92@gmail.com>
2023-02-02 07:15:43 +0000

    add node at head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,25 +13,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp_node = *head;
 	listint_t *new_node = NULL;
 	unsigned int index = 0;
+	
+	new_node = malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	while (temp_node != NULL)
 	{
 		if (idx == 0)
-		{
-			return (add_nodeint(head, n));
+		{	
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
 		}
 
 		if ((idx - index) == 1)
 		{
-
-			new_node = malloc(sizeof(listint_t));
-
-			if (new_node == NULL)
-			{
-				return (NULL);
-			}
-			new_node->n = n;
 			new_node->next = temp_node->next;
 			temp_node->next = new_node;
 			return (new_node);

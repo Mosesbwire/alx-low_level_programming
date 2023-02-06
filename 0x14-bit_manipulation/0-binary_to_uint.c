83544d92097ea7a9669ab5a536ade246b2725d9a
Mosesbwire <mosesdev92@gmail.com>
2023-02-06 15:31:05 +0000

    tasks 0 - advanced task 100

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,61 +1,37 @@
-
 #include "main.h"
+
 /**
-  * _pow_base2 - calculate power of x
-  * @x: power value
-  * Return: integer
-  */
-
-
-int _pow_base2(int x)
-{
-	int base = 2;
-	int value = 1;
-	int y;
-
-	if (x == 0)
-	{
-		return (1);
-	}
-
-	for (y = 0; y < x; y++)
-	{
-		value *= base;
-	}
-
-
-	return (value);
-}
-/**
-  * binary_to_uint - converts binary to unsigned int
-  * @b: binary value
-  * Return: decimal value or 0 on fail
-  */
-
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
-	int len = strlen(b);
-	int power = len - 1;
-	int x;
-	char ch;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
 
-	for (x = 0; x < len; x++)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (!isdigit(*b))
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
-		ch = *b;
-		value += atoi(&ch) * _pow_base2(power);
-		power--;
-		b++;
-	}
-	return (value);
 
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }

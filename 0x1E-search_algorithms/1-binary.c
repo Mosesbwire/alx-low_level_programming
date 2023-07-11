975c2c6ffa3bffe471587bd91fb5ecf020ca6b3b
Mosesbwire <mosesdev92@gmail.com>
2023-07-11 10:46:12 +0000

    Complete task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,46 +11,35 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t new_size;
+	size_t low;
+	size_t high;
 	size_t mid;
 	size_t index;
-	int *new_array;
 
-	mid = size / 2;
-	
-	printf("Searching in array: ");
-	for (index = 0; index < size; index++)
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
 	{
-		printf("%d, ", array[index]);
-	}
-	printf("\n");
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+		for (index = low; index <= high; index++)
+		{
+			printf("%d, ", array[index]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
 
-	if (array[mid] == value)
-	{
-		return (array[mid]);
+		if (array[mid] < value)
+		{
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
 	}
-
-	if (size == 1 && array[0] == value)
-	{
-		return (array[0]);
-	}
-
-	if (size == 1 && array[0] != value)
-	{
-		return (-1);
-	}
-
-	if (array[mid] < value)
-	{
-		new_array = &array[mid + 1];
-		new_size = size - (mid + 1);
-	} else {
-		new_array = array;
-		new_size = mid;
-	}
-
-	return binary_search(new_array, new_size, value);
 
 	return (-1);
-
 }
